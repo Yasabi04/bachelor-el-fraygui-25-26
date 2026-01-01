9b5be1a4caa0136a32b0a208d69b3fd9717863d0
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2026-01-01 22:14:57 +0100

    Fix: Location-Icon

--- frontend/scripts/controls.js
+++ frontend/scripts/controls.js
@@ -2,11 +2,18 @@ const centerSymbol = document.getElementById("center");
 const searchSymbol = document.getElementById("search");
 const userInput = document.getElementById("input");
 
+const userIcon = L.icon({
+    iconUrl: "./img/user-position.svg",
+    iconSize: [16, 16],
+    iconAnchor: [0, 0]
+});
+
 function getUserPermission() {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
             const { latitude, longitude } = position.coords;
-            map.flyTo([latitude, longitude], 11);
+            map.flyTo([latitude, longitude], 10);
+            L.marker([latitude, longitude], { icon: userIcon }).addTo(map);
         });
     } else {
         alert("Feature ohne Gerätestandort nicht verfügbar!");
@@ -28,13 +35,16 @@ document.addEventListener("DOMContentLoaded", (_) => {
                     const plane = Array.from(window.activePlanes.entries()).filter(
                         ([key, p]) => (key == icaoCode)
                     );
-                    console.log(plane)
-                    if (plane) {
+                    // console.log(plane)
+                    if (plane.length != 0) {
                         const lat = plane[0][1].lat
                         const lng = plane[0][1].long
                         map.flyTo([lat, lng], 7);
 
                         // isSelected hinzufügen
+                    }
+                    else {
+                        alert('Kein Flugzeug mit dieser Registrierung gefunden!')
                     }
                 }
             }
