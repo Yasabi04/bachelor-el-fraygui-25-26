09e2f214f867457a9ff236ac8c41442519201870
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2026-02-06 17:51:44 +0100

    Fix: Halbe Routen werden mit Error gehandelt

--- frontend/scripts/airplanes.js
+++ frontend/scripts/airplanes.js
@@ -1,4 +1,4 @@
-async function handlePlane(planes, start = '', end = '') {
+async function handlePlane(planes, start = "", end = "") {
     if (planes.length == 0) {
     } else if (planes.length == 1) {
         controls.style = "bottom: -100vh;";
@@ -21,113 +21,81 @@ async function handlePlane(planes, start = '', end = '') {
             aircraftLayer.setSelected(clicked);
         }
 
-        (async () => {
-            const start = await getAirport(searchPlane.dep);
-            const end = await getAirport(searchPlane.arr);
-            const progress = handleRouteProgress(
-                start.lat,
-                start.lng,
-                searchPlane.lat,
-                searchPlane.long,
-                end.lat,
-                end.lng,
-            );
-            updateInfo(
-                icao,
-                searchPlane.aircraft_type,
-                searchPlane.dep,
-                searchPlane.arr,
-                progress,
-            );
-        })();
+        handleSelectedPlane(clicked);
     } else if (planes.length > 1) {
         controls.style = "bottom: -100vh;";
 
-            flight_list.innerHTML = ""; 
-            planes.forEach((flight) => {
-                console.log(flight[0]);
-                const route = document.querySelector(".route-found-flights");
-                route.innerHTML = `${start.toUpperCase()} → ${end.toUpperCase()}`;
-                const singleFlight = document.createElement("li");
-                singleFlight.classList.add("flight-li");
-                singleFlight.innerHTML = `
+        flight_list.innerHTML = "";
+        planes.forEach((flight) => {
+            console.log(flight[0]);
+            const route = document.querySelector(".route-found-flights");
+            route.innerHTML = `${start.toUpperCase()} → ${end.toUpperCase()}`;
+            const singleFlight = document.createElement("li");
+            singleFlight.classList.add("flight-li");
+            singleFlight.innerHTML = `
                     <span class="flight-icao">${flight[0]}</span>
                 `;
 
-                singleFlight.addEventListener("click", () => {
-                    const icao = flight[0];
-                    const searchPlane = flight[1];
+            singleFlight.addEventListener("click", () => {
+                const icao = flight[0];
+                const searchPlane = flight[1];
 
-                    searchPlane.isSelected = true;
+                searchPlane.isSelected = true;
 
-                    const clicked = {
-                        icao: icao,
-                        lat: searchPlane.lat,
-                        lng: searchPlane.long,
-                        heading: searchPlane.deg || 0,
-                        type: searchPlane.aircraft_type,
-                        dep: searchPlane.dep,
-                        arr: searchPlane.arr,
-                    };
+                const clicked = {
+                    icao: icao,
+                    lat: searchPlane.lat,
+                    lng: searchPlane.long,
+                    heading: searchPlane.deg || 0,
+                    type: searchPlane.aircraft_type,
+                    dep: searchPlane.dep,
+                    arr: searchPlane.arr,
+                };
 
-                    if (aircraftLayer) {
-                        aircraftLayer.setSelected(clicked);
-                    }
+                if (aircraftLayer) {
+                    aircraftLayer.setSelected(clicked);
+                }
 
-                    (async () => {
-                        const start = await getAirport(searchPlane.dep);
-                        const end = await getAirport(searchPlane.arr);
-                        const progress = handleRouteProgress(
-                            start.lat,
-                            start.lng,
-                            searchPlane.lat,
-                            searchPlane.long,
-                            end.lat,
-                            end.lng,
-                        );
-                        updateInfo(
-                            icao,
-                            searchPlane.aircraft_type,
-                            searchPlane.dep,
-                            searchPlane.arr,
-                            progress,
-                        );
-                    })();
+                handleSelectedPlane(clicked);
 
-                    flight_list_container.style.transform =
-                        "translate(-50%, 100%)";
-                });
-
-                flight_list.appendChild(singleFlight);
+                flight_list_container.style.transform = "translate(-50%, 100%)";
             });
-            flight_list_container.style.transform = "translate(-50%, 0%)";
+
+            flight_list.appendChild(singleFlight);
+        });
+        flight_list_container.style.transform = "translate(-50%, 0%)";
     }
 }
 
 async function handleSelectedPlane(plane) {
     const flightInfo = document.querySelector(".mobile-flight-menu");
     flightInfo.style = "transform: translate(-50%, 0%)";
-    
+
     const planeLng = plane.lng || plane.long;
-    
+
     const start = await getAirport(plane.dep);
     const end = await getAirport(plane.arr);
-    
-    const progress = handleRouteProgress(
-        start.lat,
-        start.lng,
-        plane.lat,
-        planeLng,
-        end.lat,
-        end.lng,
-        plane.icao,
-    );
 
-    updateInfo(
-        plane.icao,
-        plane.aircraft_type || plane.type,
-        plane.dep,
-        plane.arr,
-        progress,
-    );
+    if (start && end) {
+        const progress = handleRouteProgress(
+            start.lat,
+            start.lng,
+            plane.lat,
+            planeLng,
+            end.lat,
+            end.lng,
+            plane.icao,
+        );
+
+        updateInfo(
+            plane.icao,
+            plane.aircraft_type || plane.type,
+            plane.dep,
+            plane.arr,
+            progress,
+        );
+    }
+    else {
+        setError('Huch!', 'Fluginformationen konnten nicht ausgelesen werden.')
+    }
 }
